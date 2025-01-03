7da782fb4ec9a857fa5b93c8e15ceeaff59b0007
marco rojouan <marcoprojouan@outlook.fr>
2025-01-03 16:19:19 +0100

    feat: logo and buttons

--- client/src/components/discoveryHeader/DiscoveryHeader.tsx
+++ client/src/components/discoveryHeader/DiscoveryHeader.tsx
@@ -3,9 +3,20 @@ import style from "./DiscoveryHeader.module.css";
 const DiscoveryHeader = () => {
   return (
     <div className={style.header}>
+      <img
+        className={style.eatIcon}
+        src="favicon/image.png"
+        alt="logo EatingNamNam"
+      />
       <p>Cuisinez saingue, Vivez biengue</p>
-      <button type="button">CONECTE TOI</button>
-      <button type="button">CREE TON COMPTE</button>
+      <div className={style.buttonsGroup}>
+        <button className={style.button} type="button">
+          CONECTE TOI
+        </button>
+        <button className={style.button} type="button">
+          CREE TON COMPTE
+        </button>
+      </div>
     </div>
   );
 };
