3430ab4d712a543d627c8f4191e267b367c704ad
marco rojouan <marcoprojouan@outlook.fr>
2025-01-03 14:55:14 +0100

    feat: routing for discoveryPage and router file

--- client/src/pages/DiscoveryPage.tsx
+++ client/src/pages/DiscoveryPage.tsx
@@ -1,7 +1,7 @@
 export default function DiscoveryPage() {
   return (
     <div>
-      <p>francis ?</p>
+      <p>Discoverypage</p>
     </div>
   );
 }
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -4,14 +4,12 @@ import DiscoveryPage from "./pages/DiscoveryPage";
 
 export const router = createBrowserRouter([
   {
-    // The root path
     element: <App />,
     children: [
       {
         path: "/",
         element: <DiscoveryPage />,
       },
-    ], // Renders the App component for the home page
+    ],
   },
-  // Try adding a new route! For example, "/about" with an About component
 ]);
