7cd91e6adb88c3a8c321409f033f607c69e063a3
Sanesthesie <victorsanlopez@gmail.com>
2025-01-07 18:28:53 +0100

    feat: create sample recipe json in a lib file

--- client/src/components/recipeCard/RecipeCard.tsx
+++ client/src/components/recipeCard/RecipeCard.tsx
@@ -2,32 +2,34 @@ import HealthRatingDisplay from "../ratingDisplay/HealthRatingDisplay";
 import UserRatingDisplay from "../ratingDisplay/UserRatingDisplay";
 import style from "./recipeCard.module.css";
 
+import sampleRecipe from "../../lib/sampleRecipe.json";
+
 export default function RecipeCard() {
   return (
-    <div className={style.cardContainer}>
-      <div className={style.recipePicture} />
-      <h2 className={style.h2}>Nom de la recette sur deux lignes</h2>
+    <article className={style.cardContainer}>
+      <figure className={style.recipePicture} />
+      <h2 className={style.h2}>{sampleRecipe.recipe1.title}</h2>
       <div className={style.infosContainer}>
         <div className={style.leftInfosContainer}>
           <UserRatingDisplay />
           <HealthRatingDisplay />
         </div>
         <div className={style.rightInfosContainer}>
-          <p className={style.rightInfo}>40 mins</p>
-          <p className={style.rightInfo}>9 ings</p>
+          <p className={style.rightInfo}>
+            {sampleRecipe.recipe1.cookingTime} mins
+          </p>
+          <p className={style.rightInfo}>
+            {sampleRecipe.recipe1.prepTime} mins
+          </p>
         </div>
       </div>
-      <p className={style.resume}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur labore
-        quis a nostrum quos, ad doloremque repellat saepe sit, facilis
-        perspiciatis quaerat debitis exercitationem blanditiis (...)
-      </p>
+      <p className={style.resume}>{sampleRecipe.recipe1.resume}</p>
       <ul className={style.labelContainer}>
         <li className={style.li} />
         <li className={style.li} />
         <li className={style.li} />
         <li className={style.li} />
       </ul>
-    </div>
+    </article>
   );
 }
