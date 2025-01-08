0bd7cb59183d3c51e49d656c08c409e7bf30b384
marco rojouan <marcoprojouan@outlook.fr>
2025-01-08 12:30:40 +0100

    fix: maj on fonts naming

--- client/src/pages/DiscoveryPage.tsx
+++ client/src/pages/DiscoveryPage.tsx
@@ -1,9 +1,11 @@
 import DiscoveryHeader from "../components/discoveryHeader/DiscoveryHeader";
+import Footer from "../components/footer/Footer";
 
 export default function DiscoveryPage() {
   return (
     <div>
       <DiscoveryHeader />
+      <Footer />
     </div>
   );
 }
