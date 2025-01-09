90d3cbb197720fe73fefc9c614209f6c0b8ce5f6
marco rojouan <marcoprojouan@outlook.fr>
2025-01-09 17:08:24 +0100

    feat: footer mobile style

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -3,38 +3,54 @@ import style from "./footer.module.css";
 
 const Footer = () => {
   return (
-    <section className={style.main}>
-      <img src="images/logo_white.png" className={style.eatlogo} alt="" />
-      <div className={style.social}>
-        <Link to="https://instagram.com/">
-          <img src="images/logo-instagram.png" className={style.icon} alt="" />
-        </Link>
-        <Link to="https://www.linkedin.com/">
-          <img src="images/logo-linkedin.png" className={style.icon} alt="" />
-        </Link>
-        <Link to="https://www.youtube.com/">
-          <img src="images/logo-youtube.png" className={style.icon} alt="" />
-        </Link>
-      </div>
-      <article className={style.footertext}>
-        <div className={style.contact}>
-          <h3>Contact :</h3>
-          <p>eatnamnam.namager@gmail.com</p>
-          <p>06-56-09-09-09.1</p>
-        </div>
-        <div className={style.mention}>
-          <h3>Mentions :</h3>
-          <p>CLUF</p>
-          <p>Conditions Générales d'Utilisation</p>
-        </div>
-        <div className={style.adress}>
-          <h3>Adresse :</h3>
-          <p>36 rue du Plougastel</p>
-          <p>34658 Rocalambourd</p>
-        </div>
-        <p className={style.copyright}> &copy; Wild Code School</p>
-      </article>
-    </section>
+    <>
+      <section className={style.main}>
+        <section className={style.logos}>
+          <img src="images/logo_white.png" className={style.eatlogo} alt="" />
+          <div className={style.social}>
+            <Link to="https://instagram.com/">
+              <img
+                src="images/logo-instagram.png"
+                className={style.icon}
+                alt=""
+              />
+            </Link>
+            <Link to="https://www.linkedin.com/">
+              <img
+                src="images/logo-linkedin.png"
+                className={style.icon}
+                alt=""
+              />
+            </Link>
+            <Link to="https://www.youtube.com/">
+              <img
+                src="images/logo-youtube.png"
+                className={style.icon}
+                alt=""
+              />
+            </Link>
+          </div>
+        </section>
+        <article className={style.footertext}>
+          <div className={style.contact}>
+            <h3>Contact :</h3>
+            <p>eatnamnam.namager@gmail.com</p>
+            <p>06-56-09-09-09.1</p>
+          </div>
+          <div className={style.mention}>
+            <h3>Mentions :</h3>
+            <p>CLUF</p>
+            <p>Conditions Générales d'Utilisation</p>
+          </div>
+          <div className={style.adress}>
+            <h3>Adresse :</h3>
+            <p>36 rue du Plougastel</p>
+            <p>34658 Rocalambourd</p>
+          </div>
+        </article>
+      </section>
+      <p className={style.copyright}> &copy; Wild Code School</p>
+    </>
   );
 };
 
