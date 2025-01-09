c5112193db75d059e91eea97dce8b8644ff76687
marco rojouan <marcoprojouan@outlook.fr>
2025-01-09 15:44:30 +0100

    feat: footer mobile style

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -4,35 +4,36 @@ import style from "./footer.module.css";
 const Footer = () => {
   return (
     <section className={style.main}>
-      <div className={style.logo}>
-        <img src="images/logo_white.png" alt="" />
-        <div className={style.social}>
-          <Link to="" className={style.icon}>
-            <img src="images/logo-instagram.png" alt="" />
-          </Link>
-          <Link to="" className={style.icon}>
-            <img src="images/logo-linkedin.png" alt="" />
-          </Link>
-          <Link to="" className={style.icon}>
-            <img src="images/logo-youtube.png" alt="" />
-          </Link>
+      <img src="images/logo_white.png" className={style.eatlogo} alt="" />
+      <div className={style.social}>
+        <Link to="https://instagram.com/">
+          <img src="images/logo-instagram.png" className={style.icon} alt="" />
+        </Link>
+        <Link to="https://www.linkedin.com/">
+          <img src="images/logo-linkedin.png" className={style.icon} alt="" />
+        </Link>
+        <Link to="https://www.youtube.com/">
+          <img src="images/logo-youtube.png" className={style.icon} alt="" />
+        </Link>
+      </div>
+      <article className={style.footertext}>
+        <div className={style.contact}>
+          <h3>Contact :</h3>
+          <p>eatnamnam.namager@gmail.com</p>
+          <p>06-56-09-09-09.1</p>
         </div>
-      </div>
-      <div>
-        <h3>Contact :</h3>
-        <p>eatnamnam.namager@gmail.com</p>
-        <p>06-56-09-09-09.1</p>
-      </div>
-      <div>
-        <h3>Mentions :</h3>
-        <p>CLUF</p>
-        <p>Conditions Générales d'Utilisation</p>
-      </div>
-      <div>
-        <h3>Mentions :</h3>
-        <p>CLUF</p>
-        <p>Conditions Générales d'Utilisation</p>
-      </div>
+        <div className={style.mention}>
+          <h3>Mentions :</h3>
+          <p>CLUF</p>
+          <p>Conditions Générales d'Utilisation</p>
+        </div>
+        <div className={style.adress}>
+          <h3>Adresse :</h3>
+          <p>36 rue du Plougastel</p>
+          <p>34658 Rocalambourd</p>
+        </div>
+        <p className={style.copyright}> &copy; Wild Code School</p>
+      </article>
     </section>
   );
 };
