fcd3e799ba0d5a429e367ed5b05d353eaf2fe506
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2025-01-22 14:08:25 +0100

    feat: add error form

--- client/src/components/ingredient/IngredientForm.tsx
+++ client/src/components/ingredient/IngredientForm.tsx
@@ -24,10 +24,24 @@ export default function IngredientForm() {
           <input
             type="text"
             className={style.bloc}
-            {...register("label", { required: true })}
+            {...register("label", {
+              required: true,
+              minLength: 3,
+              maxLength: 60,
+            })}
           />
-          {errors.label && (
+          {errors.label && errors.label.type === "required" && (
             <span className={style.error}>Champ obligatoire</span>
+          )}
+          {errors.label && errors.label.type === "minLength" && (
+            <span className={style.error}>
+              le champ doit contenir au minimum 3 caractères
+            </span>
+          )}
+          {errors.label && errors.label.type === "maxLength" && (
+            <span className={style.error}>
+              le champ doit contenir au maximun 60 caractères
+            </span>
           )}
         </label>
         <label htmlFor="protein_amount" className={style.champ}>
@@ -35,11 +49,27 @@ export default function IngredientForm() {
           <input
             type="number"
             step="0.1"
+            placeholder="0.1"
             className={style.bloc}
-            {...register("protein_amount", { required: true, minLength: 0 })}
+            {...register("protein_amount", {
+              required: true,
+              min: 0,
+              max: 100,
+            })}
           />
-          {errors.protein_amount && (
-            <span className={style.error}>Champ obligatoire</span>
+          {errors.protein_amount &&
+            errors.protein_amount.type === "required" && (
+              <span className={style.error}>Champ obligatoire</span>
+            )}
+          {errors.protein_amount && errors.protein_amount.type === "min" && (
+            <span className={style.error}>
+              Le nombre ne peut pas être négatif
+            </span>
+          )}
+          {errors.protein_amount && errors.protein_amount.type === "max" && (
+            <span className={style.error}>
+              La valeur ne doit pas excéder 100
+            </span>
           )}
         </label>
         <label htmlFor="carb_amount" className={style.champ}>
@@ -47,11 +77,22 @@ export default function IngredientForm() {
           <input
             step="0.1"
             type="number"
+            placeholder="0.1"
             className={style.bloc}
-            {...register("carb_amount", { required: true, min: 2 })}
+            {...register("carb_amount", { required: true, min: 0, max: 100 })}
           />
           {errors.carb_amount && errors.carb_amount.type === "required" && (
             <span className={style.error}>Champ obligatoire</span>
+          )}
+          {errors.carb_amount && errors.carb_amount.type === "min" && (
+            <span className={style.error}>
+              Le nombre ne peut pas être négatif
+            </span>
+          )}
+          {errors.carb_amount && errors.carb_amount.type === "max" && (
+            <span className={style.error}>
+              La valeur ne doit pas excéder 100
+            </span>
           )}
         </label>
         <label htmlFor="fat_amount" className={style.champ}>
@@ -59,11 +100,22 @@ export default function IngredientForm() {
           <input
             type="number"
             step="0.1"
+            placeholder="0.1"
             className={style.bloc}
-            {...register("fat_amount", { required: true, min: 0 })}
+            {...register("fat_amount", { required: true, min: 0, max: 100 })}
           />
-          {errors.fat_amount && (
+          {errors.fat_amount && errors.fat_amount.type === "required" && (
             <span className={style.error}>Champ obligatoire</span>
+          )}
+          {errors.fat_amount && errors.fat_amount.type === "min" && (
+            <span className={style.error}>
+              Le nombre ne peut pas être négatif
+            </span>
+          )}
+          {errors.fat_amount && errors.fat_amount.type === "max" && (
+            <span className={style.error}>
+              La valeur ne doit pas excéder 100
+            </span>
           )}
         </label>
         <label htmlFor="calorie_amount" className={style.champ}>
@@ -71,11 +123,27 @@ export default function IngredientForm() {
           <input
             type="number"
             step="0.1"
+            placeholder="0.1"
             className={style.bloc}
-            {...register("calorie_amount", { required: true, min: 0 })}
+            {...register("calorie_amount", {
+              required: true,
+              min: 0,
+              max: 999,
+            })}
           />
-          {errors.calorie_amount && (
-            <span className={style.error}>Champ obligatoire</span>
+          {errors.calorie_amount &&
+            errors.calorie_amount.type === "required" && (
+              <span className={style.error}>Champ obligatoire</span>
+            )}
+          {errors.calorie_amount && errors.calorie_amount.type === "min" && (
+            <span className={style.error}>
+              Le nombre ne peut pas être négatif
+            </span>
+          )}
+          {errors.calorie_amount && errors.calorie_amount.type === "max" && (
+            <span className={style.error}>
+              La valeur ne doit pas excéder 999
+            </span>
           )}
         </label>
         <button type="submit" className={style.btn}>
--- client/src/pages/DiscoveryPage.tsx
+++ client/src/pages/DiscoveryPage.tsx
@@ -1,10 +1,12 @@
 import DiscoveryHeader from "../components/discoveryHeader/DiscoveryHeader";
 import Footer from "../components/footer/Footer";
+import IngredientForm from "../components/ingredient/IngredientForm";
 
 export default function DiscoveryPage() {
   return (
     <div>
       <DiscoveryHeader />
+      <IngredientForm />
       <Footer />
     </div>
   );
