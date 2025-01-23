03e1080647eb88b83b43b028db88cfd54b3713a0
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2025-01-23 16:15:45 +0100

    feat: add toastify

--- client/src/components/categoryForm/CategoryForm.tsx
+++ client/src/components/categoryForm/CategoryForm.tsx
@@ -1,12 +1,37 @@
 import axios from "axios";
 import { useForm } from "react-hook-form";
+import { ToastContainer, toast } from "react-toastify";
 import type { CategoryType } from "../../lib/definitions";
-
 export default function CategoryForm() {
   const { register, handleSubmit } = useForm<CategoryType>();
 
   const formSubmit = (newCategory: CategoryType) => {
-    axios.post(`${import.meta.env.VITE_API_URL}/api/category`, newCategory);
+    axios
+      .post(`${import.meta.env.VITE_API_URL}/api/category`, newCategory)
+      .then(() => {
+        toast.success("bravo", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      })
+      .catch(() => {
+        toast.error("Erreur lors de l'ajout de la catégorie", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      });
   };
 
   return (
@@ -22,6 +47,7 @@ export default function CategoryForm() {
           />
         </label>
         <button type="submit">Ajouter</button>
+        <ToastContainer />
       </form>
     </section>
   );
