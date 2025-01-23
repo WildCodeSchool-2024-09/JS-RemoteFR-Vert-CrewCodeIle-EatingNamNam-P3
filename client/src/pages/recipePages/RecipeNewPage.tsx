dd0f016976bc88fb1216bdce5a7bf31bb98440d1
Sanesthesie <victorsanlopez@gmail.com>
2025-01-23 18:01:38 +0100

    feat: delete component from page

--- client/src/pages/recipePages/RecipeNewPage.tsx
+++ client/src/pages/recipePages/RecipeNewPage.tsx
@@ -1,11 +1,9 @@
-import DietTypeForm from "../../components/dietTypeForm/DietTypeForm";
 import RecipeForm from "../../components/recipeForm/RecipeForm";
 
 export default function RecipeNewPage() {
   return (
     <>
       <RecipeForm />
-      <DietTypeForm />
     </>
   );
 }
