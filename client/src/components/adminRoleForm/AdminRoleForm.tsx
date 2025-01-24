1aa5d6bcfd02b866d2b71e416218ae65ba14435b
marco rojouan <marcoprojouan@outlook.fr>
2025-01-24 16:29:41 +0100

    fix: set header error

--- client/src/components/adminRoleForm/AdminRoleForm.tsx
+++ client/src/components/adminRoleForm/AdminRoleForm.tsx
@@ -20,7 +20,7 @@ const AdminRoleForm = () => {
         newRole,
       );
       toast.success(response.data.message);
-    } catch {
+    } catch (err) {
       toast.error("Réessayer plus tard");
     }
   };
