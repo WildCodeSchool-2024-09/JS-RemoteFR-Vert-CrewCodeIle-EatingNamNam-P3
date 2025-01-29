0adb59db133fd07d30408844a8f2e7380cd5218d
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2025-01-29 14:27:42 +0100

    feat: style form unit type

--- client/src/components/unitTypeForm/UnitType.tsx
+++ client/src/components/unitTypeForm/UnitType.tsx
@@ -1,11 +1,17 @@
 import axios from "axios";
+import type { ReactNode } from "react";
 import { useForm } from "react-hook-form";
 import type { SubmitHandler } from "react-hook-form";
 import { ToastContainer, toast } from "react-toastify";
 import type { UnitTypeType } from "../../lib/definitions";
+import style from "./unitType.module.css";
 
 export default function UnitType() {
-  const { register, handleSubmit } = useForm<UnitTypeType>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UnitTypeType>();
 
   const formSubmit: SubmitHandler<UnitTypeType> = async (data) => {
     try {
@@ -20,19 +26,29 @@ export default function UnitType() {
   };
 
   return (
-    <section>
+    <section className={style.formunit}>
       <ToastContainer />
-      <form onSubmit={handleSubmit(formSubmit)}>
+      <form className={style.form} onSubmit={handleSubmit(formSubmit)}>
         <label htmlFor="label">
-          Nom de l'unite
+          Nom de l'unite de mesure
           <input
+            className={style.bloc}
             type="text"
             {...register("label", {
               required: "Champ obligatoire",
+              maxLength: {
+                value: 3,
+                message: "l'unité de mesure ne doit contenir que 3 caractère",
+              },
             })}
           />
+          {errors.label && (
+            <p className={style.error}>{errors.label.message as ReactNode}</p>
+          )}
         </label>
-        <button type="submit">Ajouter</button>
+        <button type="submit" className={style.btn}>
+          Ajouter
+        </button>
       </form>
     </section>
   );
