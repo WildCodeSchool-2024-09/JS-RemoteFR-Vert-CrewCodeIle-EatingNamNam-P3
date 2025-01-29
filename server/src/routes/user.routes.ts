8d58a33edd5e92c02823d428cdeb41b269412129
marco rojouan <marcoprojouan@outlook.fr>
2025-01-29 12:22:24 +0100

    feat: user validators

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
+import { validateUserSchema } from "../helpers/validators/user.validator";
 import { hashPassword } from "../middleware/hashPassword.middleware";
 import userActions from "../modules/user/userActions";
 
 const router = express.Router();
 
-router.post("/", hashPassword, userActions.add);
+router.post("/", validateUserSchema, hashPassword, userActions.add);
 
 export default router;
