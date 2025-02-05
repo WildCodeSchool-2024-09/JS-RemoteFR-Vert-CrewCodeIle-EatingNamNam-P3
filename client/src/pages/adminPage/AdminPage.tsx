6790fabaf34c1bd57bff0081fea6c7b6be6ffbe9
Sanesthesie <victorsanlopez@gmail.com>
2025-02-05 23:40:43 +0100

    feat: create a navbar for admin page

--- client/src/pages/adminPage/AdminPage.tsx
+++ client/src/pages/adminPage/AdminPage.tsx
@@ -1,28 +1,12 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import style from "./adminPage.module.css";
+import AdminNavBar from "../../components/adminNavBar/AdminNavBar";
 
 const AdminPage = () => {
   return (
     <>
       <ToastContainer />
-      <nav className={style.adminNav}>
-        <Link to="/admin/creer-role" className={style.link}>
-          Ajouter un rôle
-        </Link>
-        <Link to="/admin/creer-categorie" className={style.link}>
-          Ajouter une catégorie
-        </Link>
-        <Link to="/admin/creer-diet" className={style.link}>
-          Ajouter un type de régime
-        </Link>
-        <Link to="/admin/creer-unite" className={style.link}>
-          Ajouter une unité de mesure
-        </Link>
-        <Link to="/admin/supprimer-recette" className={style.link}>
-          Supprimer une recette
-        </Link>
-      </nav>
+      <AdminNavBar />
       <Outlet />
     </>
   );
