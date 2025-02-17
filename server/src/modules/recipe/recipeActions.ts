b690932089f618c635379889df33ba12c9ebdce1
marco rojouan <marcoprojouan@outlook.fr>
2025-02-17 16:30:19 +0100

    fix: userid when create recipe

--- server/src/modules/recipe/recipeActions.ts
+++ server/src/modules/recipe/recipeActions.ts
@@ -79,7 +79,6 @@ const add: RequestHandler = async (req, res, next) => {
       user_id: Number.parseInt(req.body.user_id),
     };
 
-    console.info(req.body);
     const insertId = await recipeRepository.create(newRecipe);
 
     req.body.recipeId = insertId;
