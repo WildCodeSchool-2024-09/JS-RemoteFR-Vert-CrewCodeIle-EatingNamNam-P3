f85d891258cd2be255714c3d8bf576d6a44a5996
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2025-02-18 15:54:33 +0100

    refactor: error message

--- client/src/validations/recipeFormValidation.ts
+++ client/src/validations/recipeFormValidation.ts
@@ -1,58 +1,58 @@
 export const recipeValidation = {
   title: {
-    required: "Ce champ est obligatoire",
+    required: "Veuillez remplir ce champ",
     minLength: {
       value: 3,
-      message: "Ce champ doit contenir au moins 3 lettres",
+      message: "Ce champ doit contenir au minimum 3 caractères",
     },
     maxLength: {
       value: 60,
-      message: "Ce champ doit contenir moins de 60 lettres",
+      message: "Ce champ doit contenir maximum 60 caractères",
     },
   },
   summary: {
-    required: "Ce champ est obligatoire",
+    required: "Veuillez remplir ce champ",
     minLength: {
       value: 40,
-      message: "Ce champ doit contenir au moins 40 lettres",
+      message: "Ce champ doit contenir au minimum 40 caractères",
     },
     maxLength: {
       value: 255,
-      message: "Ce champ doit contenir moins de 255 lettres",
+      message: "Ce champ doit contenir maximum de 255 caractères",
     },
   },
   quantity: {
     min: {
       value: 0.1,
-      message: "quantité minimun 0.1",
+      message: "Quantité minimum : 0,1",
     },
     max: {
       value: 1000,
-      message: "quantité maximun 1000 ",
+      message: "Quantité maximum : 1 000",
     },
   },
   prep_time: {
-    required: "Ce champ est obligatoire",
+    required: "Veuillez remplir ce champ",
   },
   step_order: {
     min: {
       value: 1,
-      message: "Les étapes doivent être supérieur a 1",
+      message: "Les étapes doivent être supérieures à 1",
     },
     max: {
       value: 21,
-      message: "Les étapes ne peuvent pas dépasser 21",
+      message: "Le nombre d'étapes ne peut pas dépasser 21",
     },
   },
   content: {
-    required: "Ce champ est obligatoire",
+    required: "Veuillez remplir ce champ",
     minLength: {
       value: 10,
-      message: "Les instructions doivent contenir 10 lettres minimum",
+      message: "Les instructions doivent contenir un minimum de 10 caractères",
     },
     maxLength: {
       value: 400,
-      message: "Les instructions doivent contenir 400 lettres minimum",
+      message: "Les instructions doivent contenir un maximum de 400 caractères",
     },
   },
 };
