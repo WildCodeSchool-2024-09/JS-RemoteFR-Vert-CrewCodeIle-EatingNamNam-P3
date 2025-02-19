2e6ccfd886db772b855120c0bdacb42d09e53869
Sanesthesie <victorsanlopez@gmail.com>
2025-02-19 11:44:59 +0100

    style: add responsive style to Admin Navbar

--- client/src/components/adminNavBar/AdminNavBar.tsx
+++ client/src/components/adminNavBar/AdminNavBar.tsx
@@ -3,22 +3,34 @@ import style from "./adminNavBar.module.css";
 
 export default function AdminNavBar() {
   return (
-    <nav className={style.adminNav}>
-      <Link to="/admin/creer-role" className={style.link}>
-        Ajouter un rôle
-      </Link>
-      <Link to="/admin/creer-categorie" className={style.link}>
-        Ajouter une catégorie
-      </Link>
-      <Link to="/admin/creer-diet" className={style.link}>
-        Ajouter un type de régime
-      </Link>
-      <Link to="/admin/creer-unite" className={style.link}>
-        Ajouter une unité de mesure
-      </Link>
-      <Link to="/admin/supprimer-recette" className={style.link}>
-        Supprimer une recette
-      </Link>
-    </nav>
+    <header className={style.container}>
+      <div className={style.blur}>
+        <img
+          src="/images/logo-header.png"
+          alt="Logo du site"
+          className={style.logo}
+        />
+        <h1 className={style.title}>Tableau de bord</h1>
+      </div>
+      <nav className={style.navContainer}>
+        <ul className={style.adminLinksList}>
+          <Link to="/admin/creer-role" className={style.link}>
+            Ajouter un rôle
+          </Link>
+          <Link to="/admin/creer-categorie" className={style.link}>
+            Ajouter une catégorie
+          </Link>
+          <Link to="/admin/creer-diet" className={style.link}>
+            Ajouter un type de régime
+          </Link>
+          <Link to="/admin/creer-unite" className={style.link}>
+            Ajouter une unité de mesure
+          </Link>
+          <Link to="/admin/supprimer-recette" className={style.link}>
+            Supprimer une recette
+          </Link>
+        </ul>
+      </nav>
+    </header>
   );
 }
